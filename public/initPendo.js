55e7f96b97293ef283e1b95f2a0c647046c6f77b
JingJing Jacobson <jingjing.jacobson@pendo.io>
2022-07-13 23:06:04 -0400

    switch api key

--- public/initPendo.js
+++ public/initPendo.js
@@ -2,15 +2,14 @@
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
     v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-        y=e.createElement(n);y.async=!0;y.src='https://pendo-dev-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
+        y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 
         pendo.initialize({
             visitor: {
                 id: 'visitor-934',
                 email: 'visitor934@test.com',
-                full_name: 'Sally Johnson',
-                role: 'Data Analyst'
+                full_name: 'Test Test',
             },
 
             account: {
@@ -18,4 +17,4 @@
                 name: 'Acc934'
             }
         });
-})('3337dc19-532e-4346-53c1-094ce36a3dd8');
+})('7bafe4b0-8001-44bf-521f-eec279d155ce');
