43a76a9c58e6ac2b564c64d4d29b52f73142b2f8
JingJing Jacobson <jingjing.jacobson@pendo.io>
2022-07-25 11:18:42 -0400

    add h5

--- src/views/PlaygroundView.vue
+++ src/views/PlaygroundView.vue
@@ -5,9 +5,10 @@
         <div class="flex-container">
             <v-btn elevation="2" v-on:click="showAlert('You clicked me!')">Click me!</v-btn>
             <v-btn elevation="2" id="some-id-rs12341234abcdabcd">dynamic id</v-btn>
-            <v-btn class="a-class-rs9999aaaa9999aaaa">dynamic class</v-btn>
+            <!-- <v-btn class="a-class-rs9999aaaa9999aaaa">dynamic class</v-btn> -->
             <v-btn elevation="2" id="rs123412345678abcd">invalid</v-btn>
         </div>
+        <h5 class="a-class-rs9999aaaa9999aaaa">sam's club dynamic class</h5>
     </div>
 </template>
 
