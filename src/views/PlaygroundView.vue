eaab14cffb7e83e7b9b847aa8336e14dc74806d0
JingJing Jacobson <jingjing.jacobson@pendo.io>
2022-09-19 13:18:29 -0400

    reset dynamic id

--- src/views/PlaygroundView.vue
+++ src/views/PlaygroundView.vue
@@ -4,21 +4,28 @@
         <p>Here's a playground, go and click on a bunch of things.</p>
         <div class="flex-container">
             <v-btn elevation="2" v-on:click="showAlert('You clicked me!')">Click me!</v-btn>
-            <v-btn elevation="2" id="some-id-rs12341234abcdabcd">dynamic id</v-btn>
+            <v-btn elevation="2" id="1646428942dd324">dynamic id</v-btn>
             <!-- <v-btn class="a-class-rs9999aaaa9999aaaa">dynamic class</v-btn> -->
-            <v-btn elevation="2" id="rs12341234abcdabcd">invalid</v-btn>
+            <!-- <v-btn elevation="2" id="rs12341234abcdabcd">invalid</v-btn> -->
             <v-btn elevation="2" data-meta-id="335dfe1c1f324c479a24bd8b5e1cb454">data-meta-id</v-btn>
         </div>
         <br/>
         <div class="a-class-rs9999aaaa9999aaaa" style="background-color: red; height: 30px; width: 30px;"></div>
         <br/>
-        <v-select
+        <!-- <v-select
           :items="items"
           class="dropdown"
           id="dropdown-id"
           label="Outlined style"
           outlined
-        ></v-select>
+        ></v-select> -->
+        <label for="cars">Choose a car:</label>
+        <select name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+        </select>
     </div>
 </template>
 
